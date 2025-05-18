bcc2a79bb02e8ff27763d0c09799c3757b5302e8
Jefmans <42002519+Jefmans@users.noreply.github.com>
2025-05-18 13:19:44 +0200

    updated main.py - backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
 from app.routers import health
 
 app = FastAPI()
+
+# Make FastAPI aware of reverse proxy headers
+app.add_middleware(ProxyHeadersMiddleware)
+
+# Your actual routes
 app.include_router(health.router)
