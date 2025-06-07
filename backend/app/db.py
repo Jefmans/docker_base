7e02d796493ecf94c34f01ce541db0c0913a93e8
Jefmans <42002519+Jefmans@users.noreply.github.com>
2025-06-07 15:54:16 +0200

    added Document to db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from sqlalchemy import Column, String, Integer, Table, TIMESTAMP, ARRAY
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+Base = declarative_base()
+
+class Document(Base):
+    __tablename__ = "documents"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    filename = Column(String, nullable=False)
+    title = Column(String)
+    year = Column(Integer)
+    type = Column(String)
+    topic = Column(String)
+    authors = Column(ARRAY(String))
+    isbn = Column(String)
+    doi = Column(String)
+    publisher = Column(String)
+    created_at = Column(TIMESTAMP)
 
 
 
