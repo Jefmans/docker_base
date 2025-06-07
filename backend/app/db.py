da3302e0e1895953c16e23c7dc3110ae330820d5
Jefmans <42002519+Jefmans@users.noreply.github.com>
2025-06-07 15:51:24 +0200

    updated db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 
@@ -6,3 +7,5 @@ from sqlalchemy import create_engine
 # DATABASE_URL = "postgresql://myuser:mypassword@postgres:5432/myappdb"
 DATABASE_URL = "postgresql://test:test@postgres:5432/testdb"
 engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
