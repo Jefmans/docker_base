5a711c26d32a63733cfa3f23da3a8a0786a35665
Jefmans <42002519+Jefmans@users.noreply.github.com>
2025-07-25 10:41:08 +0200

    changed elastic credentials

--- backend/app/utils/vectorstore.py
+++ backend/app/utils/vectorstore.py
@@ -1,16 +1,17 @@
 from langchain.vectorstores import ElasticsearchStore
-from langchain.embeddings import OpenAIEmbeddings
+# from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from elasticsearch import Elasticsearch
 import os
 
 def get_vectorstore(index_name: str = "pdf_chunks") -> ElasticsearchStore:
     es_client = Elasticsearch(
         hosts=[os.environ.get("ELASTIC_HOST", "http://elasticsearch:9200")],
-        basic_auth=(
-            os.environ.get("ELASTIC_USER", ""),
-            os.environ.get("ELASTIC_PASSWORD", "")
-        ),
-        verify_certs=False
+        # basic_auth=(
+        #     os.environ.get("ELASTIC_USER", ""),
+        #     os.environ.get("ELASTIC_PASSWORD", "")
+        # ),
+        # verify_certs=False
     )
 
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
